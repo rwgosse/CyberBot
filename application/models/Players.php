508579f316eae821772a305caacc0a86c989532c
XCVG <xcvgvcx@gmail.com>
2016-02-09 14:33:20 -0800

    created Players model Signed-off-by:XCVG <xcvgvcx@gmail.com>

---
+++ application/models/Players.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This is a model for players that grabs data from a MySQL database.
+ *
+ * @author Chris
+ */
+class Players extends CI_Model {
+
+
+	// Constructor
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	// retrieve a single player
+	public function get($which)
+	{
+            // get data from the database
+            $data = $this->db->get_where('players',array('player'=>$which));
+
+            // return the first and hopefully only record
+            return $data[0];
+	}
+
+	// retrieve all of the players
+	public function all()
+	{
+            // get data from the database
+            $data = $this->db->get('players');
+            
+            return $data;
+	}
+
+	// retrieve the first player
+	public function first()
+	{
+            // get data from the database
+            $data = $this->db->get('players');
+            
+            return $data[0];
+	}
+
+	// retrieve the last player
+	public function last()
+	{
+            // get data from the database
+            $data = $this->db->get('players');
+            
+            $index = count($data) - 1;
+            return $data[$index];
+	}
+
+}
