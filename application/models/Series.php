0b5944d82dcef0919afb7a7a7a60e01f97cb674c
XCVG <xcvgvcx@gmail.com>
2016-02-09 15:01:39 -0800

    created series model and test "controller" Signed-off-by:XCVG <xcvgvcx@gmail.com>

---
+++ application/models/Series.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This is a model for players that grabs data from a MySQL database.
+ *
+ * @author Chris
+ */
+class Series extends CI_Model {
+
+
+	// Constructor
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	// retrieve a single player
+	public function get($which)
+	{
+            // get data from the database
+            $data = $this->db->get_where('series',array('series'=>$which));
+
+            // return the first and hopefully only record
+            return $data->result_array()[0];
+	}
+
+	// retrieve all of the players
+	public function all()
+	{
+            // get data from the database
+            $data = $this->db->get('series');
+            
+            return $data->result_array();
+	}
+
+	// retrieve the first player
+	public function first()
+	{
+            // get data from the database
+            $data = $this->db->get('series')->result_array();
+            
+            return $data[0];
+	}
+
+	// retrieve the last player
+	public function last()
+	{
+            // get data from the database
+            $data = $this->db->get('series')->result_array();
+            
+            $index = count($data) - 1;
+            return $data[$index];
+	}
+
+}
