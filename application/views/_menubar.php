c8f541ff95118fde7935280701871cc420570a7b
XCVG <xcvgvcx@gmail.com>
2016-02-12 10:59:35 -0800

    added placeholders to login area Signed-off-by:XCVG <xcvgvcx@gmail.com>

--- application/views/_menubar.php
+++ application/views/_menubar.php
@@ -5,9 +5,9 @@
 ?>
 <h2>Cyberbots Web App</h2>
 <div id="loginbox">
-    <span id="logintext">Mickey</span>
+    <span id="logintext">{login_text}</span>
     <form name="loginform" id="loginform" autocomplete="off" method="POST">
-        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="username" placeholder="Username" style="display:{login_visibility}">
         <input type="submit" value="{submit_text}">
     </form>
 </div>
