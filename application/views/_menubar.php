652f45248500e0413e9ada4d719a211d03c1eeed
XCVG <xcvgvcx@gmail.com>
2016-02-12 10:51:35 -0800

    loginbox style and layout Signed-off-by:XCVG <xcvgvcx@gmail.com>

--- application/views/_menubar.php
+++ application/views/_menubar.php
@@ -3,9 +3,16 @@
  * Menu navbar, just an unordered list
  */
 ?>
-<ul class="nav">
-    <h2>Cyberbots Web App</h2>
+<h2>Cyberbots Web App</h2>
+<div id="loginbox">
+    <form name="loginfom" id="loginform" autocomplete="off" method="POST">
+        <input type="text" name="username" placeholder="Username">
+        <input type="submit" value="{submit_text}">
+    </form>
+</div>
+<ul class="nav">    
     {menudata}
     <li><a href="{link}">{name}</a></li>
     {/menudata}
 </ul>
+
