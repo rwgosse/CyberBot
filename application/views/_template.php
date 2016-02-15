e48db78fefab580d161fdc0125ed7a6c827c0bb0
David Wang <lijen_dwang@hotmail.com>
2016-02-14 18:45:29 -0800

    Styles:
    Added background, color changes, fonts, and sizing consistency.
    Signed-off-by:David Wang <lijen_dwang@hotmail.com>

--- application/views/_template.php
+++ application/views/_template.php
@@ -13,6 +13,7 @@ if (!defined('APPPATH'))
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
+		<link href='https://fonts.googleapis.com/css?family=Josefin+Slab:400,600' rel='stylesheet' type='text/css'>
         <title>{title}</title>
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
