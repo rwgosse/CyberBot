538a26774430bafe50dd08e2b7706f1286b43adb
XCVG <xcvgvcx@gmail.com>
2016-04-08 13:23:42 -0700

    homepage view template modified to show status Signed-off-by:XCVG <xcvgvcx@gmail.com>

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,23 +1,27 @@
 <div class="row" id="dashboard">
     <h1>Dashboard</h1>
-       <div class="dashboardContainer"> 
+    <div class="dashboardContainer"> 
         <div class="statusContainer">
-            <h3>Game Status: Round #1</h3>
+            <h3>Game Status: Round #{round-number}</h3>
+            <p>Round state: {round-state}</p>
+            <p>Time left: {round-countdown}</p>
+            <br>
+            
             <div class="rounds">
-                <div class="displayStatus">
-					{piecedisplay}
-                </div>
+                <h4>Known Pieces</h4>
+                {piecedisplay}
             </div>
+
         </div>
         <div class="playersContainer">
             <h3>Players</h3>
-			{test}
+            {test}
             <div class="homepagePlayers">
-				<a href="/Portfolio?player={player}">{player}</a><br/><br/>
-				Peanuts: {peanuts}<br/><br/>
-				Equity: {equity}<br/>
-			</div>
-			{/test}
+                <a href="/Portfolio?player={player}">{player}</a><br/><br/>
+                Peanuts: {peanuts}<br/><br/>
+                Equity: {equity}<br/>
+            </div>
+            {/test}
         </div>
-       </div>
+    </div>
 </div>
