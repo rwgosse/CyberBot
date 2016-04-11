5ccaa63b87790c4151064261eb0105dcedbe8fc4
XCVG <xcvgvcx@gmail.com>
2016-04-11 14:24:28 -0700

    cleaned up and added to homepage layout and style Signed-off-by:XCVG <xcvgvcx@gmail.com>

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,28 +1,55 @@
 <div class="row" id="dashboard">
-    <h1>Dashboard</h1>
-    <div class="dashboardContainer"> 
-        <div class="statusContainer">
-            <div class="roundStatus">
-                <h3>Game Status: Round #{round-number}</h3>
-                <p>Round state: {round-state}</p>
-                <p>Time left: {round-countdown}</p>
-            </div>
-            
-            <div class="rounds">
+    <div id="round-status">
+        <h3>Game Status: Round #{round-number}</h3>
+        <p>Round state: {round-state}</p>
+        <p>Time left: {round-countdown}</p>
+    </div>
+
+    <div id="content-container">        
+
+        <div id="content-left">
+            <h3>Pieces</h3>
+            <div id="homepage-pieces">
                 <h4>Known Pieces</h4>
                 {piecedisplay}
             </div>
 
         </div>
-        <div class="playersContainer">
+        <div id="content-right">
             <h3>Players</h3>
-            {test}
-            <div class="homepagePlayers">
-                <a href="/Portfolio?player={player}">{player}</a><br/><br/>
-                Peanuts: {peanuts}<br/><br/>
-                Equity: {equity}<br/>
+            <div>
+                {test}
+                <div class="homepage-players">
+                    <a href="/Portfolio?player={player}">{player}</a><br/><br/>
+                    Peanuts: {peanuts}<br/><br/>
+                    Equity: {equity}<br/>
+                </div>
+                {/test}
             </div>
-            {/test}
+            
+            <h3>Transactions</h3>
+            <div class="homepage-transaction">
+                <table>
+                    <tr>
+                        <th>Date/Time</th>
+                        <th>Player</th>
+                        <th>Broker</th>
+                        <th>Series</th>
+                        <th>Trans</th>
+                    </tr>
+                {transactions}
+                    <tr>
+                        <td>{datetime}</td>
+                        <td>{player}</td>
+                        <td>{broker}</td>
+                        <td>{series}</td>
+                        <td>{trans}</td>
+                    </tr>               
+                {/transactions}
+                </table>
+            </div>
+            
+            
         </div>
     </div>
 </div>
