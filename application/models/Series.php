5b6bcba9969ae383381f2e4c120ff73bc168439d
XCVG <xcvgvcx@gmail.com>
2016-04-10 17:37:29 -0700

    series model now gets data from the server Signed-off-by:XCVG <xcvgvcx@gmail.com>

--- application/models/Series.php
+++ application/models/Series.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This is a model for robot series that grabs data from a MySQL database.
+ * This is a model for robot series that grabs data from the server.
  *
  * @author Chris
  */
@@ -18,20 +18,20 @@ class Series extends CI_Model {
 	public function get($which)
 	{
             // get data from the database
-            $data = $this->db->get_where('series',$which);
+            //$data = $this->db->get_where('series',$which);
 
             // return the first and hopefully only record
-            return $data->result_array()[0];
+            return $this->get_series_where($which)[0];
 	}
         
         // retrieve all matching series
         public function get_all($which)
         {
             // get data from the database
-            $data = $this->db->get_where('series',$which);
+            //$data = $this->db->get_where('series',$which);
             
             // return all records
-            return $data->result_array();
+            return $this->get_series_where($which);
         }
 
 	// retrieve all of the series
@@ -40,14 +40,14 @@ class Series extends CI_Model {
             // get data from the database
             $data = $this->db->get('series');
             
-            return $data->result_array();
+            return $this->get_series_where(NULL);
 	}
 
 	// retrieve the first series
 	public function first()
 	{
             // get data from the database
-            $data = $this->db->get('series')->result_array();
+            $data = $this->get_series_where(NULL);
             
             return $data[0];
 	}
@@ -56,10 +56,66 @@ class Series extends CI_Model {
 	public function last()
 	{
             // get data from the database
-            $data = $this->db->get('series')->result_array();
+            $data = $this->get_series_where(NULL);
             
             $index = count($data) - 1;
             return $data[$index];
 	}
+        
+        // this function implements db get where almost perfectly
+        private function get_series_where($which)
+        {
+            //open remote URL
+            $file_handle = fopen("http://botcards.jlparry.com/data/series", "r");
+        
+            $transactions = array();
+
+            //get the first line and toss it because it's labels
+            fgetcsv($file_handle, 1024);
+            
+            //go through all remaining rows and fill an array with them
+            while (!feof($file_handle))
+            {
+                
+                $line = fgetcsv($file_handle, 1024);
+                $transaction = array('series'=>$line[0],'description'=>$line[1],'frequency'=>$line[2],'value'=>$line[3]);
+                
+                //check conditions if $which is not empty
+                if(!empty($which))
+                {
+                    $valid = TRUE;
+                    
+                    //get all array keys
+                    $keys = array_keys($which);
+                    
+                    //for each array key, check if the values match
+                    foreach($keys as $key)
+                    {
+                        //if it doesn't match, disqualify it
+                        if(!($which[$key] === $transaction[$key]))
+                        {
+                            $valid = FALSE;
+                        }
+                    }
+                    
+                    //if it's still valid, put the row in
+                    if($valid)
+                    {
+                        $transactions[] = $transaction;
+                    }
+                }
+                //if not, just put it in
+                else
+                {
+                   
+                    $transactions[] = $transaction;
+                }
+
+            }
+
+            //close the handle and return the results
+            fclose($file_handle);
+            return $transactions;
+        }
 
 }
